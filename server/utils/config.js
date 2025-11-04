108b9205bcab86eb04d2a2e72d4695589a79be5d
thnh23 <lekimthanh27062k5@gmail.com>
2025-11-05 00:41:35 +0700

    add controller, service, router for user and setting prisma

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,12 +1,5 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const SQL_CONFIG = {
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-}
 
-module.exports = {PORT, SQL_CONFIG}
+module.exports = {PORT}
